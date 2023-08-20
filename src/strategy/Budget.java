903233542b6c1cb2fc6a213495a3735c375b7282
Erick Silva <erick.ap.silva@gmail.com>
2023-08-19 22:05:32 -0300

    strategy

---
+++ src/strategy/Budget.java
@@ -0,0 +1,14 @@
+package strategy;
+
+public class Budget {
+
+    private final double value;
+
+    public Budget(double value){
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
