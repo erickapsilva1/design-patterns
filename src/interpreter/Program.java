0c2c6513f22700d2582c9b9b59734cc256b0ce45
Erick Silva <erick.ap.silva@gmail.com>
2023-08-23 22:02:06 -0300

    interpreter

--- src/interpreter/Program.java
+++ src/interpreter/Program.java
@@ -4,7 +4,7 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Expression left = new Subtraction(new Number(10), new Number(5));
+        Expression left = new Subtraction(new Sum(new Number(99), new Number(99)), new Number(1));
         Expression right = new Sum(new Number(2), new Number(10));
         Expression sum = new Sum(left, right);
 
